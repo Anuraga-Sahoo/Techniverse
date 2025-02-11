be1da3974af7078f9f1f8ec0a442df3c48362a64
Anuraga Sahoo <anuragliku.2020@gmail.com>
2025-02-11 12:45:52 +0530

    blog page responsive design complete

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import BlogBanner from '../components/blog/BlogBanner'
-
-
-
+import React from 'react';
+import BlogBanner from '../components/blog/BlogBanner';
 
 let Blogs = [
   {
@@ -40,36 +37,40 @@ let Blogs = [
     "discription": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam soluta pariatur iusto?",
     "image": "https://t3.ftcdn.net/jpg/04/24/53/50/360_F_424535039_f0HgjloFjZodjP0qDErF6HZrVm7qJ7mo.jpg",
   }
-]
+];
+
 const Blog = () => {
- 
   return (
     <>
-    <section>
-    <div className='flex justify-center items-center flex-col'>
-      <BlogBanner/>
-    </div>
-    <div className=' text-white p-[6rem] w-[100%] flex flex-wrap justify-center items-center'>
-      
-    {
-      Blogs.map(( blog, index)=>(
-        <div key={index}  onClick={() => console.log('clicked', index)} className="flex justify-center items-center flex-col bg-black text-white m-10 h-full  w-[15rem] rounded-md shadow-2xl shadow-indigo-950 cursor-pointer hover:scale-[1.1] transition-transform">
-          <div>
-            <img src={blog.image} alt="blog image" className='rounded-md' />
-          </div>
-          <div>
-            <div className='p-3'>
-              <h1 className='text-2xl font-semibold'>{blog.title}</h1>
-              <p className='text-[1rem] text-gray-600'>{blog.discription}</p>
-            </div>
+      <section>
+        {/* Blog Banner */}
+        <div className='flex justify-center items-center flex-col'>
+          <BlogBanner />
+        </div>
+
+        {/* Blog Cards Grid */}
+        <div className='text-white p-4 sm:p-8 w-full'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+            {Blogs.map((blog, index) => (
+              <div
+                key={index}
+                onClick={() => console.log('clicked', index)}
+                className="flex flex-col bg-gray-950 text-white rounded-md shadow-2xl shadow-indigo-950 cursor-pointer hover:scale-[1.05] transition-transform"
+              >
+                <div>
+                  <img src={blog.image} alt="blog" className='w-full h-48 object-cover rounded-t-md' />
+                </div>
+                <div className='p-4'>
+                  <h1 className='text-xl font-semibold'>{blog.title}</h1>
+                  <p className='text-sm text-gray-400 mt-2'>{blog.discription}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      ))
-    }
-      </div>
       </section>
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/components/blog/BlogBanner.jsx
+++ src/components/blog/BlogBanner.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const BlogBanner = () => {
   return (
-    <>
-      <div className='relative'>
-        <img src="https://www.piclumen.com/wp-content/uploads/2024/10/piclumen-marquee-06.webp" alt="banner" className='rounded-md mb-10' />
-        <div className='flex flex-col items-center gap-3 absolute top-[11rem] left-[6rem] justify-center'>
-          <h2 className='text-6xl font-bold bg-gradient-to-r from-white to-purple-900 bg-clip-text text-transparent p-3'>Explore our Blogs</h2>
-          <p className='text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-        </div>
+    <div className="relative w-full">
+      <img
+        src="https://www.piclumen.com/wp-content/uploads/2024/10/piclumen-marquee-06.webp"
+        alt="banner"
+        className="w-full h-auto rounded-md mb-10 object-cover"
+      />
+      <div className="absolute inset-x-0 top-[30%] flex flex-col items-center text-center px-4">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold bg-gradient-to-r from-white to-purple-900 bg-clip-text text-transparent p-3">
+          Explore our Blogs
+        </h2>
+        <p className="text-base sm:text-lg md:text-xl text-white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default BlogBanner
+export default BlogBanner;
